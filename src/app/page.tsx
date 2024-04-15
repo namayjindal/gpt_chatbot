3929ac9be37ba59f7fc1bc3c3930e23e48042bba
Namay Jindal <namayjindal@nyu.edu>
2024-04-15 20:19:32 +0200

    Initial Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import React from "react"
+import Chat from "@/chat"
+
+export const runtime = 'edge'
 
 export default function Home() {
-  return (
-    <section className="py-24">
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold text-center">Welcome to my website</h1>
-        <p className="text-center mt-4">This is a simple website built with React and Tailwind CSS</p>
-      </div>
-    </section>
-  )
+  return <Chat />
 }
---
+++ src/chat.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+export default function Chat() {
+    return (
+        <section className="text-zinc-700">
+            <div className="container flex h-screen flex-col items-center justify ">
+                <h1 className="font-serif text-2xl font-medium">AI Chatbot</h1>
+                <div className='mt-4 w-full max-w-lg'>
+                    {/* response container */}
+
+                    {/* input form */}
+                </div>
+            </div>
+        </section>
+    )
+}
